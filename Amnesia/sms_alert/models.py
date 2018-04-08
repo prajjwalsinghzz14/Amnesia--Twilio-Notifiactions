45bfa71ac8edb7e57feb8b9a603b640322b85f26
prajjwalsinghzz14 <prajjwalsinghzz14@gmail.com>
2018-04-09 00:44:46 +0530

    Update models.py

--- Amnesia/sms_alert/models.py
+++ Amnesia/sms_alert/models.py
@@ -13,22 +13,25 @@ import arrow
 class User(models.Model):
     name = models.CharField(max_length=50)
     sleep_time = models.TimeField(blank=False, default=datetime.now())
-    wake_time = models.DateTimeField(blank=False, default=datetime.now())
+    wake_time = models.TimeField(blank=False, default=datetime.now())
     time_zone = TimeZoneField(default='US/Pacific')
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=False)
     country = CountryField()
-
+    
+    # Additional fields not visible to users
+    task_id = models.CharField(max_length=50, blank=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return self.name + ' - ' + self.phone_number
 
 
 
     def schedule_reminder(self):
-        """Schedules a Celery task to send a reminder about this appointment"""
-
-        # Calculate the correct time to send this reminder
+       
+        # Calculate the correct time to send this sms-alert
         sms_time = arrow.get(self.wake_time, self.time_zone.zone)
         reminder_time = sms_time.replace(minutes=+settings.REMINDER_TIME)
 
@@ -37,3 +40,5 @@ class User(models.Model):
         result = send_sms_reminder.apply_async((self.pk,), eta=reminder_time)
 
         return result.id
+    
+        
