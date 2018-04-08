e42529a6b1ce1d72e0e279c670102cbc433543fe
prajjwalsinghzz14 <prajjwalsinghzz14@gmail.com>
2018-04-09 00:45:33 +0530

    Update urls.py

--- Amnesia/sms_alert/urls.py
+++ Amnesia/sms_alert/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.index, name='index'),
-    #url(r'^d/\d+/$',views.get, name='get'),
+    url(r'^form_save.html$', views.form_save, name="form_save"),
     url(r'^next.html$', views.next, name="next"),
 
 ]
