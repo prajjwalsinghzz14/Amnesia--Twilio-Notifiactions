5051f14f4eb820dc192c5b51dfa9ba68e75820f4
prajjwalsinghzz14 <prajjwalsinghzz14@gmail.com>
2018-04-09 00:47:19 +0530

    Update views.py

--- Amnesia/sms_alert/views.py
+++ Amnesia/sms_alert/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render, HttpResponseRedirect, redirect, HttpResponse
+from django.shortcuts import render, HttpResponse
 from .forms import UserForm
 from sms_alert.models import User
-
-temp_pk=0;
-temp=User()
 
 def index(request):
 
@@ -11,8 +8,6 @@ def index(request):
         name = request.POST.get('name')
         phone_number = request.POST.get('phone_number')
         country = request.POST.get('country')
-        #sleep_time = request.POST.get('sleep_time')
-        #wake_time = request.POST.get('wake_time')
         user_form = UserForm({
             'timezone': 'America/Los_Angeles',
         })
@@ -20,16 +15,13 @@ def index(request):
 
         temp = User(
             name = name,
-          #  sleep_time=sleep_time,
-           # wake_time=wake_time,
             time_zone = time_zone,
             phone_number = phone_number,
             country = country
         )
-        temp_pk = temp.pk;
-        temp.save()
+
         return HttpResponse("<h1>Successfully entered</h1>")
-        #return render(request, 'sms_alert/index.html' )
+
 
     else:
         form = UserForm()
@@ -39,16 +31,41 @@ def index(request):
 def next(request):
 
     if request.method == 'POST':
-
-        sleep_time = request.POST.get('sleep_time')
-        wake_time = request.POST.get('wake_time')
-
-        temp = User(
-            sleep_time = sleep_time,
-            wake_time = wake_time,
-        )
-        temp.save()
-        return HttpResponse("<h1>Successfully entered</h1>")
+        form = UserForm(request.POST)
+        time_zone = request.POST.get('timezone')
+        name = request.POST.get('name')
+        phone_number = request.POST.get('phone_number')
+        country = request.POST.get('country')
+        return render(request, 'sms_alert/next.html', {
+            'name' : name,
+            'phone_number' : phone_number,
+            'country' : country,
+            'time_zone' : time_zone,
+        })
 
     else:
-        return render(request, 'sms_alert/index.html')
+        return render(request, 'sms_alert/next.html')
+
+def form_save(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone_number = request.POST.get('phone_number')
+        country = request.POST.get('country')
+        time_zone = request.POST.get('time_zone')
+        awake_time = request.POST.get('awake_time')
+        sleep_time = request.POST.get('sleep_time')
+        print("-----------------------------------------------------", request.POST)
+        temp = User(
+            name=name,
+            sleep_time=sleep_time,
+            wake_time=awake_time,
+            time_zone=time_zone,
+            phone_number=phone_number,
+            country=country
+        )
+        temp.save()
+        form = UserForm()
+        return render(request, 'sms_alert/index.html', {'form': form})
+    else:
+        form = UserForm()
+        return render(request, 'sms_alert/index.html', {'form': form})
